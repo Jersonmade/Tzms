121f5de97e8e6032a76aefc53d9a00ec6cb3546d
Jersonmade <dima.sergeev.0404@inbox.ru>
2024-03-17 11:03:33 +0400

    Changed GlobalExceptionHandler

--- src/main/java/com/example/Tzms/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/Tzms/advice/GlobalExceptionHandler.java
@@ -10,9 +10,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Глобальный обработчик исключений
+ */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * Метод, обрабатывающий исключения, возникающие при создании продукта,
+     * когда переданные аргументы невалидны.
+     * @param ex
+     * @return
+     */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
@@ -24,6 +33,12 @@ public class GlobalExceptionHandler {
         return errorMap;
     }
 
+    /**
+     * Метод, обрабатывающий исключения, возникающие при передачи
+     * несуществующего идентификатора продукта
+     * @param ex
+     * @return
+     */
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(ProductNotFoundException.class)
     public Map<String, String> handleProductNotFound(ProductNotFoundException ex) {
